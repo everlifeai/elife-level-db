29715035c9a7091de6acfb21602ccb7d5e000815
Productive Programmer <charles.lobo@gmail.com>
2018-10-24 20:22:27 +1100

    Move data to `../elife-data` folder

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 'use strict'
 const cote = require('cote')({statusLogsEnabled:false})
 const level = require('level')
+const u = require('elife-utils')
 
-const db = level('./elife.db')
-
-/*      understand/
- * The database microservice (partitioned by key `everlife-db-svc` to
- * prevent conflicting with other services.
- */
-const dbService = new cote.Responder({
-    name: 'Everlife DB Service',
-    key: 'everlife-db-svc',
+u.ensureExists('/data/level.db', (err, dbdir) => {
+    if(err) u.showErr(err)
+    else startLevelDB(dbdir)
 })
 
-/*      outcome/
- * Responds to a `put` request by saving values into the database
- * TODO: Security/partitioning for various services
- */
-dbService.on('put', (req, cb) => {
-    if(!req.key) cb('No key to save in DB')
-    else db.put(req.key, req.val, cb)
-})
+function startLevelDB(dbdir) {
+    const db = level(dbdir)
 
-/*      outcome/
- * Responds to a `get` request by getting values from the database
- */
-dbService.on('get', (req, cb) => {
-    if(!req.key) cb('Need key to get from DB')
-    else db.get(req.key, cb)
-})
+    /*      understand/
+     * The database microservice (partitioned by key `everlife-db-svc` to
+     * prevent conflicting with other services.
+     */
+    const dbService = new cote.Responder({
+        name: 'Everlife DB Service',
+        key: 'everlife-db-svc',
+    })
 
+    /*      outcome/
+     * Responds to a `put` request by saving values into the database
+     * TODO: Security/partitioning for various services
+     */
+    dbService.on('put', (req, cb) => {
+        if(!req.key) cb('No key to save in DB')
+        else db.put(req.key, req.val, cb)
+    })
+
+    /*      outcome/
+     * Responds to a `get` request by getting values from the database
+     */
+    dbService.on('get', (req, cb) => {
+        if(!req.key) cb('Need key to get from DB')
+        else db.get(req.key, cb)
+    })
+}
 
